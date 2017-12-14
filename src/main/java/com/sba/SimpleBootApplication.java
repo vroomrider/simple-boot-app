4d0286a263c43fe3dd96a522e2a064dd7d309626
PradeepNaikR <pradeep.naik@riflexions.com>
2017-12-14 11:32:22 +0530

    Added the project files!

---
+++ src/main/java/com/sba/SimpleBootApplication.java
@@ -0,0 +1,12 @@
+package com.sba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBootApplication.class, args);
+	}
+}
